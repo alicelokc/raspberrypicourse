8723bfc3af4dd43e3573cb7a21d564bc6ea4f8cc
alicelokc <67314582+alicelokc@users.noreply.github.com>
2021-02-04 15:53:14 +0800

    Add files via upload

---
+++ measuring_distance.py
@@ -0,0 +1,54 @@
+import RPi.GPIO as GPIO
+import time
+
+from luma.core.interface.parallel import bitbang_6800
+from luma.core.render import canvas
+from luma.oled.device import ssd1306, ssd1309, ssd1325, ssd1331, sh1106, ws0010
+
+serial = i2c(port=1, address=0x3C)
+
+# substitute ssd1331(...) or sh1106(...) below if using that device
+device = sh1106(serial)
+
+try:
+    while True:
+        GPIO.setmode(GPIO.BOARD)
+
+        PIN_TRIGGER = 16
+        PIN_ECHO = 18
+
+        GPIO.setup(PIN_TRIGGER, GPIO.OUT)
+        GPIO.setup(PIN_ECHO, GPIO.IN)
+
+        GPIO.output(PIN_TRIGGER, GPIO.LOW)
+
+        print ("Waiting for sensor to settle")
+
+        time.sleep(2)
+
+        print ("Calculating distance")
+
+        GPIO.output(PIN_TRIGGER, GPIO.HIGH)
+
+        time.sleep(0.00001)
+
+        GPIO.output(PIN_TRIGGER, GPIO.LOW)
+
+        while GPIO.input(PIN_ECHO)==0:
+            pulse_start_time = time.time()
+        while GPIO.input(PIN_ECHO)==1:
+            pulse_end_time = time.time()
+
+        pulse_duration = pulse_end_time - pulse_start_time
+        distance = round(pulse_duration * 17150, 2)
+        print ("Distance:",distance,"cm")
+        result = str(distance)
+
+        with canvas(device) as draw:
+            draw.rectangle(device.bounding_box, outline="white", fill="black")
+            draw.text((20, 10), "Distance:" , fill="white")
+            draw.text((20, 20), result, fill="white")
+            draw.text((70, 20), "cm", fill="white")
+
+finally:
+      GPIO.cleanup()
---
+++ timelapse_camera.py
@@ -0,0 +1,8 @@
+from time import sleep 
+import picamera 
+
+WAIT_TIME = 3
+with picamera.PiCamera() as camera:
+    camera.resolution = (1024, 768)
+    for filename in camera.capture_continuous('/home/pi/time-lapse/img{timestamp:%H-%M-%S-%f}.jpg'):
+        sleep(WAIT_TIME)
