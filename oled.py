96b254144ac63d0b2b03f7db867451b7aabe2e5a
alicelokc <67314582+alicelokc@users.noreply.github.com>
2021-02-04 18:21:01 +0800

    Update oled.py

--- oled.py
+++ oled.py
@@ -1,3 +1,4 @@
+from luma.core.interface.serial import i2c, spi, pcf8574
 from luma.core.interface.parallel import bitbang_6800
 from luma.core.render import canvas
 from luma.oled.device import ssd1306, ssd1309, ssd1325, ssd1331, sh1106, ws0010
@@ -10,6 +11,7 @@ serial = i2c(port=1, address=0x3C)
 # substitute ssd1331(...) or sh1106(...) below if using that device
 device = sh1106(serial)
 
-with canvas(device) as draw:
-    draw.rectangle(device.bounding_box, outline="white", fill="black")
-    draw.text((30, 40), "Hello World", fill="white")
+while True:
+    with canvas(device) as draw:
+        draw.rectangle(device.bounding_box, outline="white", fill="black")
+        draw.text((30, 40), "Hello World", fill="white")
