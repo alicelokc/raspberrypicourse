1da0d44d52e5f4e865efaf59518ed9ec7338a2f4
alicelokc <67314582+alicelokc@users.noreply.github.com>
2021-02-04 16:12:24 +0800

    Update measuring_distance.py

--- measuring_distance.py
+++ measuring_distance.py
@@ -1,6 +1,7 @@
 import RPi.GPIO as GPIO
 import time
 
+from luma.core.interface.serial import i2c, spi, pcf8574
 from luma.core.interface.parallel import bitbang_6800
 from luma.core.render import canvas
 from luma.oled.device import ssd1306, ssd1309, ssd1325, ssd1331, sh1106, ws0010
